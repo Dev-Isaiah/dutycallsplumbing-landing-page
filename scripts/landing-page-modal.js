1a0d4fcacca968bd1a59e5e722f185eb7e4a6d06
Isaiah Brown <irbrown54321@gmail.com>
2023-01-09 04:47:22 -0500

    Completed Service, Banner, and Footer sections

--- scripts/landing-page-modal.js
+++ scripts/landing-page-modal.js
@@ -1,14 +1,42 @@
-// const servicesModal = document.getElementById("servicesModal");
+const servicesModal = document.getElementById("services-modal");
+const modalContent = document.getElementsByClassName("modal-content")[0];
 const servicesInfoBox = document.getElementsByClassName("services-info-box");
-// const btnClose = document.getElementsByClassName(".btn-close");
+const btnClose = document.getElementsByClassName("btn-close")[0];
+const modalInformation = document.getElementsByClassName("modal-information");
 
-console.log(servicesInfoBox);
-// console.log(servicesModal);
+// FUNCTIONS
+function showModal() {
+    servicesModal.style.display = "flex";
+    modalContent.style.animation = "fade-in 0.5s ease";
+}
 
-// for(let i = 0; i < servicesInfoBox.length; i++){
-//     console.log(i);
-// }
+function hideModal() {
+    setTimeout(() => {
+        servicesModal.style.display = "none";
+        for (const informationBoxes of modalInformation) {
+            informationBoxes.style.display = "none";
+        }
+    }, 190);
+    modalContent.style.animation = "fade-out 0.2s ease";
+}
 
-// function test(){
-//     alert("OPEN THE MODAL!");
-// }
+
+// CALL FUNCTIONS
+for (const infoBoxes of servicesInfoBox) {
+
+    let infoBoxesTitle = infoBoxes.getElementsByTagName('h4')[0].innerHTML;
+    infoBoxes.onclick = function () {
+        showModal();
+        for (const information of modalInformation) {
+            let informationTitle = information.getElementsByTagName('h3')[0].innerHTML;
+
+            if (infoBoxesTitle === informationTitle) {
+                information.style.display = "block";
+            }
+        }
+    }
+}
+
+btnClose.onclick = function () {
+    hideModal();
+}
